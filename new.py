5631ad4965d288bc5f33db497e7eb93284d1d7ec
Marek Holík <marekholik01@gmail.com>
2024-04-15 10:50:21 +0000

    update

--- new.py
+++ new.py
@@ -1,16 +1,22 @@
 import random
 
 def main():
-    percent = 10
+    percent = 90
     with open("picture.pgm", "r") as f:
         f.readline()
         width, height = map(int, f.readline().split())
         picture = [[int(f.readline()) for _ in range(int(width))] for _ in range(int(height))]
     display_matrix = [[1 for _ in range(int(width))] for _ in range(int(height))]
-    for _ in range(int((height*width)/percent)):
-        random_x = random.randint(0, int(width)-1)
-        random_y = random.randint(0, int(height)-1)
-        display_matrix[random_y][random_x] = 0
+    counter = 0
+    while counter < int(width)* int(height)/100 * percent:
+        random_x = random.randint(1, int(width)-2)
+        random_y = random.randint(1, int(height)-2)
+        if display_matrix[random_y][random_x] == 0:
+            continue
+        else:  
+            counter += 1
+            display_matrix[random_y][random_x] = 0
+            
     with open("new_picture.pgm", "w") as f:
         f.write("P2\n")
         f.write(str(width) + " " + str(height) + "\n")
@@ -18,7 +24,8 @@ def main():
         for i in range(int(height)):
             for j in range(int(width)):
                 f.write(str(picture[i][j] * display_matrix[i][j]) + "\n")   
-        
+                
+    
     return 0
 
 
